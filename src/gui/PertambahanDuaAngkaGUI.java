e77e2ae9ab65dfc6d49de1bc4bac552950cc643f
maowgit-lab <46674852+maowgitlab@users.noreply.github.com>
2022-11-02 17:23:39 +0800

    Menambahkan kode program tambah

--- src/gui/PertambahanDuaAngkaGUI.java
+++ src/gui/PertambahanDuaAngkaGUI.java
@@ -5,6 +5,8 @@
  */
 package gui;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Admin
@@ -220,6 +222,17 @@ public class PertambahanDuaAngkaGUI extends javax.swing.JFrame {
 
     private void tambahBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tambahBtnActionPerformed
         // TODO add your handling code here:
+        if(angkaPertamaTF.getText().equals("") || angkaKeduaTF.getText().equals("")) {
+//            dialog jika inputan kosong
+            JOptionPane.showMessageDialog(null, "Salah satu input tidak boleh kosong!");
+        } else {
+            int angkaPertama = Integer.valueOf(angkaPertamaTF.getText());
+            int angkaKedua   = Integer.valueOf(angkaKeduaTF.getText());
+
+            int hasil        = angkaPertama + angkaKedua;
+
+            hasilTF.setText(Integer.toString(hasil));
+        }
     }//GEN-LAST:event_tambahBtnActionPerformed
 
     /**
